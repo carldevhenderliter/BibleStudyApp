49e151e895f337ced04909662455c2cd4eb04f52
carldevhenderliter <artdesignsbycarl@gmail.com>
2025-11-12 21:43:27 -0500

    Update main.tsx

--- client/main.tsx
+++ client/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./src/App";
-import "./client/index.css";
+import "./src/index.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(<App />);
